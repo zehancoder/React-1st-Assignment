ed07e40aa581bfedeb89950e9582e7c2aad53e0f
zehancoder <zehanjh93@gmail.com>
2025-01-27 21:49:29 +0600

    footer copy write text added

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,7 +4,7 @@ function FooterFunc() {
             <section>
                 <div className=" md:px-12 px-4 bg-[#07292F] md:py-14 py-12 flex items-center sm:flex-row flex-col justify-between mx-auto">
                     <img className="w-24" src="/darkLogo.png" alt="" />
-                    
+                    <h2 className="md:text-xl text-lg font-extralight work-sens text-[#96ACAF] sm:mt-0 mt-5">Copyright © 2023 Wizia. All rights reserved.</h2>
                 </div>
             </section>
         </>
